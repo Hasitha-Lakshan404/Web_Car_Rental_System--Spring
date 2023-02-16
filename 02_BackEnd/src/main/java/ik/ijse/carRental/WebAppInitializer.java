f53b1a030f42368439a882a30fd9ab37d9520eb0
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2023-02-16 22:41:12 +0530

    webAppInitializer

---
+++ 02_BackEnd/src/main/java/ik/ijse/carRental/WebAppInitializer.java
@@ -0,0 +1,28 @@
+package ik.ijse.carRental;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :CarRentalSystem
+ * Date :2/16/2023
+ * Time :10:37 PM
+ */
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
