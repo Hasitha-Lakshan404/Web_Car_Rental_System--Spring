bdbb59cb2ac48e979d2a81b8aabaef6417a7409a
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2023-02-17 00:01:30 +0530

    ReservationController

---
+++ 02_BackEnd/src/main/java/lk/ijse/carRental/controller/ReservationController.java
@@ -0,0 +1,19 @@
+package lk.ijse.carRental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :CarRentalSystem
+ * Date :2/17/2023
+ * Time :12:00 AM
+ */
+
+@RestController
+@RequestMapping("/reservation")
+@CrossOrigin
+public class ReservationController {
+
+}
