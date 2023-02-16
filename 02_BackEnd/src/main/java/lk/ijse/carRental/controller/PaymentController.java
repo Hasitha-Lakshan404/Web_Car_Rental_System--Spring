f1e3a4885688fe61181eacd18bc646d417b3cd1e
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2023-02-17 00:00:39 +0530

    paymentController

---
+++ 02_BackEnd/src/main/java/lk/ijse/carRental/controller/PaymentController.java
@@ -0,0 +1,18 @@
+package lk.ijse.carRental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :CarRentalSystem
+ * Date :2/16/2023
+ * Time :11:59 PM
+ */
+
+@RestController
+@RequestMapping("/payment")
+@CrossOrigin
+public class PaymentController {
+}
